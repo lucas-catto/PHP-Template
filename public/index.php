b388b407ec66013f8587f8c68f26d6e02ba904fe
lucas-catto <lucascattosilva2018@gmail.com>
2024-02-04 19:10:54 -0800

    How Output Buffer work

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo 'home';
+$data = ['name'=>'lucas', 'age'=>'16'];
+
+ob_start();
+
+extract($data);
+
+require ('home.php');
+
+echo 'Home';
+
+$content = ob_get_contents();
+
+ob_end_clean();
+
+var_dump($content);
